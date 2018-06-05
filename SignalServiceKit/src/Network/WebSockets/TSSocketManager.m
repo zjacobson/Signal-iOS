432f0c7261eb2e345963d34886a693e7a88ae04e
Matthew Chen <matthew@signal.org>
2018-06-05 13:37:45 -0600

    Experiment with swift protos/

--- SignalServiceKit/src/Network/WebSockets/TSSocketManager.m
+++ SignalServiceKit/src/Network/WebSockets/TSSocketManager.m
@@ -24,7 +24,7 @@
 #import "TextSecureKitEnv.h"
 #import "Threading.h"
 #import "WebSocketResources.pb.h"
-//#import "SignalServiceKit-Swift.h"
+#import "SignalServiceKit-Swift.h"
 
 //@import SignalServiceKit;
 
