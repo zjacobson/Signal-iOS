dee1af94d393224b21eef7d8176bf0459c8f2723
Matthew Chen <matthew@signal.org>
2018-08-23 13:57:32 -0400

    Fix broken tests.

--- SignalServiceKit/tests/Util/OWSAnalyticsTests.m
+++ SignalServiceKit/tests/Util/OWSAnalyticsTests.m
@@ -1,8 +1,8 @@
 //
-//  Copyright (c) 2017 Open Whisper Systems. All rights reserved.
+//  Copyright (c) 2018 Open Whisper Systems. All rights reserved.
 //
 
-#import "NSData+Base64.h"
+#import "NSData+OWS.h"
 #import "OWSAnalytics.h"
 #import <XCTest/XCTest.h>
 
